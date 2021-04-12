621195286756f4acaaa7c4f787711d8261a910b7
syedshehriyar <69626096+syedshehriyar@users.noreply.github.com>
2021-04-12 15:13:21 +0530

    input output processing done

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 var btnTranslate = document.querySelector(".btn-translate");
-console.log(btnTranslate);
-
 var txtArea = document.querySelector("#txt-area");
-console.log(txtArea);
-
-//Approach1
-// btnTranslate.addEventListener("click", function clickEvenetHandler() {
-// 	console.log("clicked");
-// });
-
-//Approach2
+var outputdiv = document.querySelector("#output");
 function clickHandler() {
-	console.log("Hey clicky");
-	console.log("input", txtArea.value);
+	outputdiv.innerText = "this is a transated text" + txtArea.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
-
-//Approach3
-// btnTranslate.addEventListener("click", () => {
-// 	console.log("clidked in arrow");
-// });
