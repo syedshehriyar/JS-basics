59b42f91ecc37954d3d3e7108b099a30412a141a
syedshehriyar <69626096+syedshehriyar@users.noreply.github.com>
2021-04-24 17:13:46 +0530

    feat: new functionalities

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var btnTranslate = document.querySelector(".btn-translate");
-var txtArea = document.querySelector("#txt-area");
+var txtInput = document.querySelector("#txt-area");
 var outputdiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/dothraki.json";
+
+function constructUrl(text) {
+	return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+	console.log("error occured" + error);
+	alert("Something wrong with server! try again later");
+}
 function clickHandler() {
-	outputdiv.innerText = "this is a transated text" + txtArea.value;
+	//taking input
+	var inputText = txtInput.value;
+
+	//processing
+	fetch(constructUrl(inputText))
+		.then((response) => response.json())
+		.then((json) => {
+			var translatedText = json.contents.translated;
+			outputdiv.innerText = translatedText;
+		})
+		.catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
